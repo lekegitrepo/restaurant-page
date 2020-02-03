7002151f319c25160494aa40a4842972f76dc12f
lekegitrepo <lekedevrepo@gmail.com>
2020-02-03 16:58:24 -0600

    set event listener for the tabs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import 'bootstrap';
 import './scss/app.scss';
-import {content, mImage} from './modules/app.js';
+import {content} from './modules/app.js';
 
 const mainContent = document.getElementById('content');
 
 mainContent.appendChild(content);
 
-const brand = document.getElementById('brand');
-brand.style.color = 'white';
+const nav_menu = document.getElementById('nav_menu');
+ if (nav_menu) { console.log('nav_menu is loaded')}
 
 const container = document.createElement('div');
 container.className = 'container mt-5';
@@ -17,14 +17,26 @@ row.className = 'row';
 
 const descrip = document.createElement('div');
 descrip.className = 'col-4';
-descrip.textContent = 'This is Description';
 row.appendChild(descrip);
 
 const conten_img = document.createElement('img');
 conten_img.className = 'col-8';
-conten_img.src = mImage;
 row.appendChild(conten_img);
 
 container.appendChild(row);
 
 content.appendChild(container);
+nav_menu.addEventListener('click', (e) => {
+  const elem = e.target
+  console.log(elem.getAttribute('data_description'))
+  descrip.textContent = elem.getAttribute('data_description')
+  conten_img.src = elem.getAttribute('data_image')
+});
+
+const brand = document.getElementById('brand');
+brand.style.color = 'white';
+brand.addEventListener('load', () => {
+  console.log('onload is working!')
+  descrip.textContent = brand.getAttribute('data_description')
+  conten_img.src = brand.getAttribute('data_image')
+});
--- src/modules/app.js
+++ src/modules/app.js
@@ -1,4 +1,4 @@
-// import image from '../../images/image-1.png';
+import image from '../../images/image-1.png';
 import {Menu} from './components/menu.js';
 import {SpecialMenu} from './components/special_menu.js';
 import {Contact} from './components/contact.js';
@@ -10,24 +10,31 @@ const content = document.createElement('div');
 const navbar = document.createElement('div');
 navbar.className = 'navbar navbar-expand-lg bg-dark fixed-top';
 
+function setAttributes(el, attrs) {
+  Object.keys(attrs).forEach(key => el.setAttribute(key, attrs[key]));
+}
+
 //brand
 const brand = document.createElement('a');
 brand.className = 'navbar-brand';
 brand.textContent = 'Brand';
-brand.setAttribute('id', 'brand');
+setAttributes(brand, { id: 'brand',  data_image: image.toString(), href: '#',
+              data_description: 'This is a good restaurant'});
 navbar.appendChild(brand);
 
 const nav_menu = document.createElement('ul');
 nav_menu.className = 'navbar-nav ml-auto';
+nav_menu.setAttribute('id', 'nav_menu');
 
 //menu tab
 const li_menu = document.createElement('li');
 li_menu.className = 'nav-item';
 const li_menu_a = document.createElement('a');
+setAttributes(li_menu_a, {data_image: Menu.getImage(), data_description: 'This is a good menu'});
 li_menu_a.className = 'nav-link';
-li_menu_a.setAttribute('href', '#');
 li_menu_a.textContent = Menu.getName();
 li_menu.appendChild(li_menu_a);
+
 nav_menu.appendChild(li_menu);
 
 //special menu tab
@@ -35,9 +42,11 @@ const li_special= document.createElement('li');
 li_special.className = 'nav-item';
 const li_special_a = document.createElement('a');
 li_special_a.className = 'nav-link';
-li_special_a.setAttribute('href', '#');
 li_special_a.textContent = SpecialMenu.getName();
 li_special.appendChild(li_special_a);
+
+setAttributes(li_special_a, {data_image: SpecialMenu.getImage(), data_description: 'This is a good menu'});
+
 nav_menu.appendChild(li_special);
 
 //contact tab
@@ -45,9 +54,11 @@ const li_contact = document.createElement('li');
 li_contact.className = 'nav-item';
 const li_contact_a = document.createElement('a');
 li_contact_a.className = 'nav-link';
-li_contact_a.setAttribute('href', '#');
 li_contact_a.textContent = Contact.getName();
 li_contact.appendChild(li_contact_a);
+
+setAttributes(li_contact_a, {data_image: Contact.getImage(), data_description: 'This is our contact'});
+
 nav_menu.appendChild(li_contact);
 
 //about tab
@@ -55,13 +66,16 @@ const li_about = document.createElement('li');
 li_about.className = 'nav-item';
 const li_about_a = document.createElement('a');
 li_about_a.className = 'nav-link';
-li_about_a.setAttribute('href', '#');
 li_about_a.textContent = About.getName();
 li_about.appendChild(li_about_a);
+
+setAttributes(li_about_a, {data_image: About.getImage(),
+              data_description: 'This is about how we start cooking good food'});
+
 nav_menu.appendChild(li_about);
 
 navbar.appendChild(nav_menu);
 
 content.appendChild(navbar)
 
-export {content, mImage};
+export {content};
