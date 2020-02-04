b7eb00006d67e6deadfd5bfc64f5d08772d4f01b
lekegitrepo <lekedevrepo@gmail.com>
2020-02-04 12:23:47 -0600

    fix stickler errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import 'bootstrap';
 import './scss/app.scss';
-import { content } from './modules/app.js';
+import { content } from './modules/app';
 
 const mainContent = document.getElementById('content');
 
 mainContent.appendChild(content);
 
-const nav_menu = document.getElementById('nav_menu');
+const navMenu = document.getElementById('nav_menu');
 
 // container for display each tab content.
 const container = document.createElement('div');
@@ -19,9 +18,9 @@ const descrip = document.createElement('div');
 descrip.className = 'col-4';
 row.appendChild(descrip);
 
-const conten_img = document.createElement('img');
-conten_img.className = 'col-8';
-row.appendChild(conten_img);
+const contenImg = document.createElement('img');
+contenImg.className = 'col-8';
+row.appendChild(contenImg);
 
 container.appendChild(row);
 
@@ -29,23 +28,23 @@ content.appendChild(container);
 
 const linkTags = (currElem) => {
   const a = document.querySelectorAll('.navbar a');
-  const att = [...a].map(el => { if (el.hasAttribute('style')) { el.setAttribute('style', ''); } });
+  [...a].map(el => { if (el.hasAttribute('style')) { el.setAttribute('style', ''); } });
   currElem.setAttribute('style', 'border-bottom: 2px solid #c73232; background-color: #343a40 !important; border-radius: 20% 0px 0px;');
 };
 
-nav_menu.addEventListener('click', (e) => {
+navMenu.addEventListener('click', (e) => {
   const elem = e.target;
   descrip.innerHTML = elem.getAttribute('data_description');
-  conten_img.src = elem.getAttribute('data_image');
+  contenImg.src = elem.getAttribute('data_image');
   linkTags(elem);
 });
-
-function load() {
-  descrip.innerHTML = brand.getAttribute('data_description');
-  conten_img.src = brand.getAttribute('data_image');
-  linkTags(brand);
-}
 
 const brand = document.getElementById('brand');
 brand.onload = load();
 brand.addEventListener('click', load);
+
+function load() {
+  descrip.innerHTML = brand.getAttribute('data_description');
+  contenImg.src = brand.getAttribute('data_image');
+  linkTags(brand);
+}
