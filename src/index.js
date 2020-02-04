2f600940f891c83f52dff111a1be110f253190d2
lekegitrepo <lekedevrepo@gmail.com>
2020-02-04 12:16:21 -0600

    fix stickler errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import 'bootstrap';
 import './scss/app.scss';
-import {content} from './modules/app.js';
+import { content } from './modules/app.js';
 
 const mainContent = document.getElementById('content');
 
 mainContent.appendChild(content);
 
 const nav_menu = document.getElementById('nav_menu');
- if (nav_menu) { console.log('nav_menu is loaded')}
 
-//container for display each tab content.
+// container for display each tab content.
 const container = document.createElement('div');
 container.className = 'container';
 
@@ -30,26 +29,23 @@ content.appendChild(container);
 
 const linkTags = (currElem) => {
   const a = document.querySelectorAll('.navbar a');
-  const att = [...a].map(el => {if (el.hasAttribute('style')) {el.setAttribute('style', '')}})
+  const att = [...a].map(el => { if (el.hasAttribute('style')) { el.setAttribute('style', ''); } });
   currElem.setAttribute('style', 'border-bottom: 2px solid #c73232; background-color: #343a40 !important; border-radius: 20% 0px 0px;');
-}
+};
 
 nav_menu.addEventListener('click', (e) => {
-  const elem = e.target
-  console.log(elem.getAttribute('data_description'))
-  descrip.innerHTML = elem.getAttribute('data_description')
-  conten_img.src = elem.getAttribute('data_image')
-  linkTags(elem)
+  const elem = e.target;
+  descrip.innerHTML = elem.getAttribute('data_description');
+  conten_img.src = elem.getAttribute('data_image');
+  linkTags(elem);
 });
 
-function load (){
-  console.log('onload is working!')
-  descrip.innerHTML = brand.getAttribute('data_description')
-  conten_img.src = brand.getAttribute('data_image')
+function load() {
+  descrip.innerHTML = brand.getAttribute('data_description');
+  conten_img.src = brand.getAttribute('data_image');
   linkTags(brand);
 }
 
 const brand = document.getElementById('brand');
-if (brand) { console.log('brand is loaded')}
 brand.onload = load();
 brand.addEventListener('click', load);
