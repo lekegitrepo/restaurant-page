4d6827140148c482855f1bc2a0b3cfae86b33554
lekegitrepo <lekedevrepo@gmail.com>
2020-02-04 12:40:50 -0600

    fix stickler errors

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,8 +1,8 @@
-import {HomePage} from './components/homepage.js';
-import {Menu} from './components/menu.js';
-import {SpecialMenu} from './components/special_menu.js';
-import {Contact} from './components/contact.js';
-import {About} from './components/about.js';
+import { HomePage } from './components/homepage.js';
+import { Menu } from './components/menu.js';
+import { SpecialMenu } from './components/special_menu.js';
+import { Contact } from './components/contact.js';
+import { About } from './components/about.js';
 
 const mImage = Menu.getImage();
 const content = document.createElement('div');
@@ -14,42 +14,45 @@ function setAttributes(el, attrs) {
   Object.keys(attrs).forEach(key => el.setAttribute(key, attrs[key]));
 }
 
-//brand
+// brand
 const brand = document.createElement('a');
 brand.className = 'navbar-brand';
 brand.textContent = HomePage.getName();
-setAttributes(brand, { id: 'brand',  data_image: HomePage.getImage(),
-              data_description: `${HomePage.getDescription()}`});
+setAttributes(brand, {
+  id: 'brand',
+  data_image: HomePage.getImage(),
+  data_description: `${HomePage.getDescription()}`,
+});
 navbar.appendChild(brand);
 
 const nav_menu = document.createElement('ul');
 nav_menu.className = 'navbar-nav ml-auto';
 nav_menu.setAttribute('id', 'nav_menu');
 
-//menu tab
+// menu tab
 const li_menu = document.createElement('li');
 li_menu.className = 'nav-item';
 const li_menu_a = document.createElement('a');
-setAttributes(li_menu_a, {data_image: Menu.getImage(), data_description: Menu.getDescription()});
+setAttributes(li_menu_a, { data_image: Menu.getImage(), data_description: Menu.getDescription() });
 li_menu_a.className = 'nav-link';
 li_menu_a.textContent = Menu.getName();
 li_menu.appendChild(li_menu_a);
 
 nav_menu.appendChild(li_menu);
 
-//special menu tab
-const li_special= document.createElement('li');
+// special menu tab
+const li_special = document.createElement('li');
 li_special.className = 'nav-item';
 const li_special_a = document.createElement('a');
 li_special_a.className = 'nav-link';
 li_special_a.textContent = SpecialMenu.getName();
 li_special.appendChild(li_special_a);
 
-setAttributes(li_special_a, {data_image: SpecialMenu.getImage(), data_description: SpecialMenu.getDescription()});
+setAttributes(li_special_a, { data_image: SpecialMenu.getImage(), data_description: SpecialMenu.getDescription() });
 
 nav_menu.appendChild(li_special);
 
-//contact tab
+// contact tab
 const li_contact = document.createElement('li');
 li_contact.className = 'nav-item';
 const li_contact_a = document.createElement('a');
@@ -57,11 +60,11 @@ li_contact_a.className = 'nav-link';
 li_contact_a.textContent = Contact.getName();
 li_contact.appendChild(li_contact_a);
 
-setAttributes(li_contact_a, {data_image: Contact.getImage(), data_description: Contact.getDescription()});
+setAttributes(li_contact_a, { data_image: Contact.getImage(), data_description: Contact.getDescription() });
 
 nav_menu.appendChild(li_contact);
 
-//about tab
+// about tab
 const li_about = document.createElement('li');
 li_about.className = 'nav-item';
 const li_about_a = document.createElement('a');
@@ -69,13 +72,15 @@ li_about_a.className = 'nav-link';
 li_about_a.textContent = About.getName();
 li_about.appendChild(li_about_a);
 
-setAttributes(li_about_a, {data_image: About.getImage(),
-              data_description: About.getDescription()});
+setAttributes(li_about_a, {
+  data_image: About.getImage(),
+  data_description: About.getDescription(),
+});
 
 nav_menu.appendChild(li_about);
 
 navbar.appendChild(nav_menu);
 
-content.appendChild(navbar)
+content.appendChild(navbar);
 
-export {content};
+export { content };
